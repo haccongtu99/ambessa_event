3a9c7cf7789cb6097d0ee7aa06c52f9e3c19ccc8
Vinh Nguyen <vinhnguyetan99@gmail.com>
2024-11-16 23:12:33 +0700

    feat: update logic drawer

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,45 +8,27 @@ function isMobileScreen() {
   return window.innerWidth <= 938;
 }
 
-
-// function isMobileDevice() {
-//   if (
-//     (navigator.userAgent.match(/Android/i))
-//     || navigator.userAgent.match(/webOS/i)
-//     || navigator.userAgent.match(/iPhone/i)
-//     || navigator.userAgent.match(/iPod/i)
-//     || navigator.userAgent.match(/BlackBerry/i)
-//     || navigator.userAgent.match(/Windows Phone/i)
-//   ) {
-//     return true;
-//   }
-//   return false;
-// };
-
 function handleMode() {
   isMobileFlag = isMobileScreen() || isMobileDevice();
   if (!isMobileFlag) {
     $('#header').removeClass('header-mobile');
-    $('#drawer').addClass('d-none');
   } else {
     $('#header').addClass('header-mobile');
-    $('#drawer').removeClass('d-none');
   }
 }
 
 function handleDrawer() {
   $('.btn-menu-list').click(() => {
+    if ($('#drawer').css('display') === 'none') {
+      $('#drawer').css('display', 'block');
+    }
     $('#drawer').addClass('drawer-animate-fadein');
     $('#drawer').removeClass('drawer-animate-fadeout');
-    // $('#drawer').addClass('animate__slideInLeft');
-    // $('#drawer').removeClass('animate__slideOutLeft');
   })
 
   $('.btn-close-drawer').click(() => {
     $('#drawer').addClass('drawer-animate-fadeout');
     $('#drawer').removeClass('drawer-animate-fadein');
-    // $('#drawer').removeClass('animate__slideInLeft');
-    // $('#drawer').addClass('animate__slideOutLeft');
   });
 }
 
